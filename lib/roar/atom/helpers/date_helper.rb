30d6a82fbe748af3f16d06ee9bf85f5e3cf77fc4
Fanny Cheung <fanny@ynote.hk>
2016-03-17 11:24:49 +0100

    Simplify date helper method

--- lib/roar/atom/helpers/date_helper.rb
+++ lib/roar/atom/helpers/date_helper.rb
@@ -2,14 +2,7 @@ module Roar
   module Atom
     class DateHelper
       def self.to_rfc3339(date)
-        unless date.respond_to?(:to_datetime) || date.respond_to?(:rfc3339)
-          fail NoMethodError,
-               "undefined method `to_datetime` or `rfc3339` for #{date}"
-        end
-
-        date = date.to_datetime if date.kind_of?(Time)
-
-        date.rfc3339
+        date.to_datetime.rfc3339
       end
 
       def self.is_rfc3339_format?(value)
